4d159ced5d097fe6a51896f1e25b2c02528bd436
Scott Mitchell <smitche60@gmail.com>
2017-12-07 21:33:32 -0800

    Create valid query string

--- query.py
+++ query.py
@@ -13,21 +13,27 @@ import sys
 
 parser = argparse.ArgumentParser(prog='PROG')
 
-parser.add_argument('-f', '--format', default = 'csv')
+parser.add_argument('-f', '--format', default = 'tablular')
 parser.add_argument('-c', '--column', nargs = '*', default = '*')
 parser.add_argument('-e', '--engine', default = 'presto')
 parser.add_argument('db_name')
 parser.add_argument('table_name')
-
-# parser.add_argument('-l', '--limit', default = 'NULL')
-# parser.add_argument('-m', '--min', default = 'NULL')
-# parser.add_argument('-M', '--MAX', default = 'NULL')
+parser.add_argument('-l', '--limit', default = 'NULL')
+parser.add_argument('-m', '--min', default = 'NULL')
+parser.add_argument('-M', '--MAX', default = 'NULL')
 
 args = parser.parse_args(sys.argv[1:])
 
-print(args.format, args.column, args.engine)
-print(args.db_name, args.table_name)
+query = 'SELECT ' + ' '.join(args.column) + ' FROM ' + args.table_name + ' '
 
+if args.min != 'NULL' and args.MAX != 'NULL':
+    time_range = "WHERE TD_TIME_RANGE(timestamp," + args.min + "," + args.MAX + ")"
+    query += time_range
+
+if args.limit != 'NULL':
+    query += ' LIMIT ' + args.limit
+
+print(query)
 
 # with tdclient.Client(config.TD_API_KEY) as td:
 #     job = td.query("cities", "SELECT * FROM world_cities LIMIT 5", type="hive")
